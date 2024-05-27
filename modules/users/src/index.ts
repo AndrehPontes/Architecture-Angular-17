eefc33e58f77a0523f64de70f2159a6694745e46
Carlos André Batalha Pontes <carlos.pontes@itriad.org.br>
2024-05-27 00:21:04 -0400

    [CAA-015] list service users

--- modules/users/src/index.ts
+++ modules/users/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/users/users.component';
+export * from './lib/users/user-detail/user-detail.component'
--- modules/users/src/lib/users/users.component.ts
+++ modules/users/src/lib/users/users.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserService } from './shared/user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'lib-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
 })
-export class UsersComponent {}
+export class UsersComponent {
+  users$ = this.userService.getUsers();
+
+  constructor(private userService: UserService) {}
+}
